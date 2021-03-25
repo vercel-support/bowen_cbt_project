ee6a7ac559fdb17b097f12e393b904080ad72470
ayodeji334 <fawumiayodeji@gmail.com>
2021-03-25 20:37:18 +0100

    add the font url

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,8 @@ export default function Home() {
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap" rel="stylesheet"></link>
       </Head>
 
       <main className={styles.main}>
